3d15af524d39473237f6425e9429de6d42b88350
deros <derose.luca@gmail.com>
2020-04-29 11:52:17 +0200

    Fixed issues

--- Dataset2Image/lib/DeepInsight_train_norm.py
+++ Dataset2Image/lib/DeepInsight_train_norm.py
@@ -195,7 +195,7 @@ def train_norm(param, dataset, norm):
         #                                custom_cut=range(0, image_model["custom_cut"]))
         XTestGlobal = [ConvPixel(dataset["Xtest"][:, i], np.array(image_model["xp"]), np.array(image_model["yp"]),
                                  image_model["A"], image_model["B"], custom_cut=range(0, image_model["custom_cut"]))
-                       for i in range(0, dataset["Xtest"].shape[1]))]  # dataset["Xtest"].shape[1])]
+                       for i in range(0, dataset["Xtest"].shape[1])]  # dataset["Xtest"].shape[1])]
 
         print("Test Images done!")
 
